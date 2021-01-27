1e0e037f66218ecc68d53ea680e91eb76077b023
Nina Jensen <jnik@aarhus.dk>
2021-01-27 15:57:38 +0100

    Add upload_file method - WIP

--- acastorage/main.py
+++ acastorage/main.py
@@ -1,6 +1,7 @@
 # -----------------------------------------------------------------------------
 # Imports
 # -----------------------------------------------------------------------------
+from pathlib import Path
 from typing import Any
 
 from azure.storage.blob.aio import ContainerClient
@@ -26,3 +27,16 @@ class ACAStorage(ContainerClient):
         )
         # TODO: Implement exists() check when MS adds it, cf.
         # https://github.com/Azure/azure-sdk-for-python/pull/16315
+
+    async def upload(self, source: Path, dest: Path) -> None:
+        if source.is_dir():
+            pass
+        elif source.is_file():
+            await self.upload_file(source, dest)
+
+    async def upload_file(self, source: Path, dest: Path) -> None:
+        if not source.is_file():
+            raise ValueError(f"Source {source} must be a file.")
+
+        with source.open("rb") as data:
+            await self.upload_blob(name=str(dest), data=data)
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,7 @@
 # Imports
 # -----------------------------------------------------------------------------
 import os
+from pathlib import Path
 
 import pytest
 
@@ -18,3 +19,9 @@ class TestACAStorage:
 
     async def test_init(self):
         assert ACAStorage("test", credential=self.cred)
+
+    async def test_upload_file(self, temp_dir):
+        test_file: Path = temp_dir / "test.txt"
+        test_file.write_text("Testing")
+        test_storage = ACAStorage("test", credential=self.cred)
+        await test_storage.upload_file(test_file, Path(".") / test_file.name)
