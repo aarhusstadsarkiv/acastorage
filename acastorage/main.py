62312a41fe2a19bc0887e5f0d70eafe571aa645d
Nina Jensen <jnik@aarhus.dk>
2021-02-01 15:29:08 +0100

    Implement blob upload (#10)
    * Add upload_file method - WIP
    
    * Add UploadError custom exception
    
    * Update upload_file with error handling, docstrings
    
    * Use SAS token in testing
    
    * Update test to delete blob after upload
    
    * Move deletion of blobs to function

--- acastorage/main.py
+++ acastorage/main.py
@@ -1,9 +1,12 @@
 # -----------------------------------------------------------------------------
 # Imports
 # -----------------------------------------------------------------------------
+from pathlib import Path
 from typing import Any
 
 from azure.storage.blob.aio import ContainerClient
+
+from acastorage.exceptions import UploadError
 
 # -----------------------------------------------------------------------------
 # Main ACAStorage Class
@@ -26,3 +29,30 @@ class ACAStorage(ContainerClient):
         )
         # TODO: Implement exists() check when MS adds it, cf.
         # https://github.com/Azure/azure-sdk-for-python/pull/16315
+
+    async def upload_file(self, source: Path, dest: Path = Path(".")) -> None:
+        """Upload source file to a specified destination. The destination
+        is always assumed to be a directory, and defaults to the container
+        root.
+
+        Parameters
+        ----------
+        source : pathlib.Path
+            The source file to upload.
+        dest: pathlib.Path, optional
+            The destination folder to upload to. Defaults to pathlib.Path(".").
+
+        Raises
+        ------
+        OSError
+            If the source is not a file.
+        """
+        if not source.is_file():
+            raise ValueError(f"Source {source} is not a file.")
+
+        with source.open("rb") as data:
+            upload_dest = dest / source.name
+            try:
+                await self.upload_blob(name=str(upload_dest), data=data)
+            except Exception as err:
+                raise UploadError(f"Upload of {source} failed with {err}")
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,7 @@
 # Imports
 # -----------------------------------------------------------------------------
 import os
+from pathlib import Path
 
 import pytest
 
@@ -13,8 +14,19 @@ from acastorage import ACAStorage
 pytestmark = pytest.mark.asyncio
 
 
+async def delete(storage: ACAStorage) -> None:
+    await storage.delete_blobs(*[b async for b in storage.list_blobs()])
+
+
 class TestACAStorage:
-    cred = os.getenv("ACASTORAGE_KEY")
+    cred = os.getenv("ACASTORAGE_TOKEN")
 
     async def test_init(self):
         assert ACAStorage("test", credential=self.cred)
+
+    async def test_upload_file(self, temp_dir):
+        test_file: Path = temp_dir / "test.txt"
+        test_file.write_text("Testing")
+        test_storage = ACAStorage("test", credential=self.cred)
+        await test_storage.upload_file(test_file)
+        await delete(test_storage)
