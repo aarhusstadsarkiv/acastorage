2e0344a17078f4e192c1395fb7966abf966bb6ed
Nina Jensen <jnik@aarhus.dk>
2021-02-02 12:16:56 +0100

    Rename dest parameter in upload_file (#15)
    * Change dest parameter to dest_dir
    
    * Change ValueError to FileNotFound error
    
    * Update test of main ACAStorage -> full coverage

--- acastorage/main.py
+++ acastorage/main.py
@@ -30,7 +30,7 @@ class ACAStorage(ContainerClient):
         # TODO: Implement exists() check when MS adds it, cf.
         # https://github.com/Azure/azure-sdk-for-python/pull/16315
 
-    async def upload_file(self, source: Path, dest: Path = Path(".")) -> None:
+    async def upload_file(self, source: Path, dest_dir: Path) -> None:
         """Upload source file to a specified destination. The destination
         is always assumed to be a directory, and defaults to the container
         root.
@@ -39,19 +39,22 @@ class ACAStorage(ContainerClient):
         ----------
         source : pathlib.Path
             The source file to upload.
-        dest: pathlib.Path, optional
-            The destination folder to upload to. Defaults to pathlib.Path(".").
+        dest_dir: pathlib.Path
+            The destination folder to upload to.
 
         Raises
         ------
-        OSError
+        FileNotFoundError
             If the source is not a file.
+        UploadError
+            If upload of the file fails. Reraises exceptions from
+            Azure's ContainerClient in a more user-friendly format.
         """
         if not source.is_file():
-            raise ValueError(f"Source {source} is not a file.")
+            raise FileNotFoundError(f"Source {source} is not a file.")
 
         with source.open("rb") as data:
-            upload_dest = dest / source.name
+            upload_dest = dest_dir / source.name
             try:
                 await self.upload_blob(name=str(upload_dest), data=data)
             except Exception as err:
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,8 +5,10 @@ import os
 from pathlib import Path
 
 import pytest
+from azure.storage.blob.aio import ContainerClient
 
 from acastorage import ACAStorage
+from acastorage.exceptions import UploadError
 
 # -----------------------------------------------------------------------------
 # Tests
@@ -28,5 +30,26 @@ class TestACAStorage:
         test_file: Path = temp_dir / "test.txt"
         test_file.write_text("Testing")
         test_storage = ACAStorage("test", credential=self.cred)
-        await test_storage.upload_file(test_file)
+        await test_storage.upload_file(test_file, Path("test"))
         await delete(test_storage)
+
+    async def test_file_not_found(self, temp_dir):
+        test_file: Path = temp_dir / "test_not_found.txt"
+        test_storage = ACAStorage("test", credential=self.cred)
+        err_match = f"Source {test_file} is not a file"
+        with pytest.raises(FileNotFoundError, match=err_match):
+            await test_storage.upload_file(test_file, Path("test"))
+
+    async def test_upload_error(self, temp_dir, monkeypatch):
+        err_msg = "fail"
+
+        async def upload_error(*args, **kwargs):
+            raise Exception(err_msg)
+
+        monkeypatch.setattr(ContainerClient, "upload_blob", upload_error)
+        test_file: Path = temp_dir / "test.txt"
+        test_file.write_text("Testing")
+        test_storage = ACAStorage("test", credential=self.cred)
+        err_match = f"Upload of {test_file} failed with {err_msg}"
+        with pytest.raises(UploadError, match=err_match):
+            await test_storage.upload_file(test_file, Path("test"))
